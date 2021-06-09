ee19cc24996aca9623225d0fcd9a53028b901ff0
Mateusz <echelon133@gmail.com>
2021-06-09 16:39:39 +0200

    Add main app class

---
+++ src/main/java/ml/echelon133/microbloggateway/MicroblogGatewayApplication.java
@@ -0,0 +1,13 @@
+package ml.echelon133.microbloggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroblogGatewayApplication.class, args);
+	}
+
+}
